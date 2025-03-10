cc69545b49a2af9d2ad2d476c86a43133a2344cb
abhirawat10 <abhi10rawat2002@gmail.com>
2025-03-10 23:47:52 +0530

    seperating contact class

--- Program.cs
+++ Program.cs
@@ -13,34 +13,10 @@ namespace AddressBook
 
             //adding new contact
             Contact contact1 = new Contact("John", "Doe", "123 Main ", "Springfield", "NY", "62701", "217-555-1234", "john@gmai.com");
-            AddressBook.add(contact1);
+            AddressBook.Add(contact1);
             Console.WriteLine("new contact added");
         }
     }
 
-    internal class Contact
-    {
-
-        string firstName { get; set; }
-        string lastName { get; set; }
-        string address { get; set; }
-        string city { get; set; }
-        string state { get; set; }
-        string zip { get; set; }
-        string phone { get; set; }
-        string email { get; set; }
-
-        public Contact(string firstName, string lastName, string address, string city, string state, string zip, string phone, string email)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.address = address;
-            this.city = city;
-            this.state = state;
-            this.zip = zip;
-            this.phone = phone;
-            this.email = email;
-        }
-    }
 
 }
